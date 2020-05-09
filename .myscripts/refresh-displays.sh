5554210b1e60d5041235321f2fc63f450fb4a42e
bstee615 <benjaminjsteenhoek@gmail.com>
2020-05-08 21:21:32 -0500

    Add refresh script

--- .myscripts/refresh-displays.sh
+++ .myscripts/refresh-displays.sh
@@ -1,12 +1,22 @@
+#!/bin/bash
+
+killandwait(){
+  # Kill
+  killall -q $1 
+  
+  # Wait until the processes have been shut down
+  while pgrep -u $UID -x $1 >/dev/null; do sleep 1; done
+}
+
 # Picom
-killall -q picom
+killandwait picom
 picom -b
 
 # Wallpaper
-killall -q feh
-feh --bg-scale --no-fehbg ~/.bg/bg.png &
+killandwait feh
+feh --bg-fill --no-fehbg ~/.bg/bg.png &
 
 # Polybar
-#killall -q polybar
-~/.config/polybar/launch.sh &
+killandwait polybar
+polybar mybar &
 
